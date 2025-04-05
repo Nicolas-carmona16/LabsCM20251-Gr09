7f816ac4d9cbb3f506284398e7eb4d768c323b72
Nicolas <carmonanicolas74mail.com>
2025-04-04 21:22:31 -0500

    Lab1 First commit

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20251/lab1/ui/theme/Theme.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr09_20251/lab1/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package co.edu.udea.compumovil.gr09_20251.lab1.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
