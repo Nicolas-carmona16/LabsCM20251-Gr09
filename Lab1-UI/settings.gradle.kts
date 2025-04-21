539ecb63a486cfdf085be1a88873983c4c798419
cripto26 <daniel4826073@gmail.com>
2025-04-20 19:09:00 -0500

    Corrijo errores y configuración del proyecto

---
+++ Lab1-UI/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs")
+    }
+}
+
+rootProject.name = "Lab1-UI"
